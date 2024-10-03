660789daeaa5a369ed6c096490a76f5a64bf78f2
Jerome <jeromelesne@gmail.com>
2024-10-04 01:16:06 +0200

    wezterm : changed padding and font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,13 +7,18 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 config.enable_tab_bar = false
 config.font = wezterm.font {
-    family = "Hack Nerd Font Mono",
+    family = "FiraCode Nerd Font Mono",
+    weight = "Medium"
 }
-config.font_size = 19
+config.font_size = 18
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
-config.window_padding = { bottom = 0 }
+config.window_padding = {
+    left = "10px",
+    right = 0,
+    top = "10px",
+    bottom = "10px",
+}
 
--- and finally, return the configuration to wezterm
 return config
