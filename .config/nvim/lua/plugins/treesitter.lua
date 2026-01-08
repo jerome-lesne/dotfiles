b0f935f5f6760431f88cb83d29852c72f2c4d873
jerome <jeromelesne@gmail.com>
2026-01-08 12:10:54 +0100

    nvim : reformatting

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,41 @@
 local config = function()
-    require('nvim-treesitter.configs').setup({
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'angular' },
-        auto_install = true,
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { 'ruby' },
-        },
-        indent = { enable = true, disable = { 'ruby' } },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<Leader>s", -- set to `false` to disable one of the mappings
-                node_incremental = "si",
-                scope_incremental = "sc",
-                node_decremental = "sd",
-            },
-        },
-    })
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"angular",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = { "ruby" },
+			disable = { "dockerfile" },
+		},
+		indent = { enable = true, disable = { "ruby" } },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<Leader>s", -- set to `false` to disable one of the mappings
+				node_incremental = "si",
+				scope_incremental = "sc",
+				node_decremental = "sd",
+			},
+		},
+	})
 end
 
 return {
-    'nvim-treesitter/nvim-treesitter',
-    branch = "master",
-    build = ':TSUpdate',
-    config = config
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
+	config = config,
 }
